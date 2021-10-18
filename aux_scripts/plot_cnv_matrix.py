0a8804c30ec15d7ffdcfb7060e1a51358949372a
adigenova <digenova@gmail.com>
2021-10-18 08:54:21 +0200

    updating CNV plot

--- aux_scripts/plot_cnv_matrix.py
+++ aux_scripts/plot_cnv_matrix.py
@@ -1,5 +1,4 @@
-from SigProfilerMatrixGenerator.scripts import CNVMatrixGenerator as scna
-from SigProfilerExtractor import sigpro as sig
+import sigProfilerPlotting as sigPlt
 import sys, getopt
 import pandas as pd
 
@@ -23,16 +22,19 @@ if __name__ == '__main__':
   if opt in ("-h"):
     print ("run_cnv_matrix.py -m <cnv_matrix> -n <name_project> -o <output_path>")
     sys.exit()
-elif opt in ("-m","--matrix"):
-   cnv_file=arg
-elif opt in ("-n","--project"):
-   prefix=arg
-elif opt in ("-o","--output_path"):
-   output_path=arg
-elif opt in ("-p","--percentaje"):
-   percentage=True
-elif opt in ("-a","--agregate"):
+  elif opt in ("-m","--matrix"):
+    matrix_file=arg
+  elif opt in ("-n","--project"):
+    project=arg
+  elif opt in ("-o","--output_path"):
+    output_path=arg
+  elif opt in ("-p","--percentaje"):
+    percentage=True
+  elif opt in ("-a","--agregate"):
     aggregate=True
 
+print(matrix_file)
+print(output_path)
+print(project)
 #we call the function for ploting the copy number
-sigPlt.plotCNV(matrix_path, output_path, project, "pdf", percentage=percentage, aggregate=aggregate) #plotting of CNV counts
+sigPlt.plotCNV(matrix_file, output_path, project, "pdf", percentage=percentage, aggregate=aggregate) #plotting of CNV counts
